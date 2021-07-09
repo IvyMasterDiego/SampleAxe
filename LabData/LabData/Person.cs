2432953c879f14d4938b5b654b44f1b19ea84737
IvyMasterDiego <diegolobato82@gmail.com>
2021-07-08 23:33:25 -0400

    ejemplo

---
+++ LabData/LabData/Person.cs
@@ -0,0 +1,158 @@
+﻿using LabWork;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace OOP
+{
+    public sealed class Person
+    {
+        public static List<Person> persons = new List<Person>();
+
+        public string Id { get; }
+        public string Name { get; }
+        public string Apellido { get; }
+        public double Ahorros;
+        public string Password { get; }
+        private int bitpacking = 0;
+        public int Age => (bitpacking >> 4);
+        public Gender gender => (Gender)(bitpacking & 0b1000);
+        public MStatus mStatus => (MStatus)(bitpacking & 0b100);
+        public AGrad aGrad => (AGrad)(bitpacking & 0b11);
+
+
+        public Person(in string id, in string name, in string apellido, in int age, in double ahorros, in Gender gender, in MStatus mStatus, in AGrad aGrad, in string password)
+        {
+            Id = id;
+            Name = name;
+            Apellido = apellido;
+            Ahorros = ahorros;
+            Password = password;
+            bitpacking = (age << 4) | (int)gender | (int)mStatus | (int)aGrad;
+        }
+        public override string ToString() => $"{GetType().Name}({nameof(Id)}:{Id}; {nameof(Name)}:{Name}; {nameof(Apellido)}:{Apellido}; {nameof(Age)}:{Age}; {nameof(Gender)}:{gender}; {nameof(MStatus)}:{mStatus}; {nameof(AGrad)}:{aGrad}; {nameof(Ahorros)}:{Ahorros}; {nameof(Password)}:{Password}; {nameof(bitpacking)}:{bitpacking};)";
+
+
+
+        internal static Person FromCsvLine(string line)
+        {
+            string[] tokens = line.Split(',');
+            (string id,
+                string name,
+                string apellido,
+                double ahorros,
+              int bitpacking,
+                string password
+                ) =
+                (
+                tokens[0],
+                tokens[1],
+                tokens[2],
+                double.Parse(tokens[3]),
+                int.Parse(tokens[4]),
+                tokens[5]
+                );
+            int age = (bitpacking >> 4);
+            Gender gender = (Gender)(bitpacking & 0b1000);
+            MStatus mStatus = (MStatus)(bitpacking & 0b100);
+            AGrad aGrad = (AGrad)(bitpacking & 0b11);
+            return new Person(id, name, apellido, age, ahorros, gender, mStatus, aGrad, password);
+        }
+
+        internal static Person FromConsole(string record)
+        {
+            var tokens = record.Split(',');
+            (string id,
+                string name,
+                string apellido,
+                int age,
+                double ahorros,
+                string password,
+                Gender gender,
+                MStatus mStatus,
+                AGrad aGrad
+                ) =
+                (
+                tokens[0],
+                tokens[1],
+                tokens[2],
+                int.Parse(tokens[3]),
+                double.Parse(tokens[4]),
+                tokens[5],
+                (Gender)int.Parse(tokens[6]),
+            (MStatus)int.Parse(tokens[7]),
+            (AGrad)int.Parse(tokens[8])
+                );
+            return new Person(id, name, apellido, age, ahorros, gender, mStatus, aGrad, password);
+        }
+
+        internal static void SaveToCsv()
+        {
+            if (persons.Count() > 0)
+            {
+                File.WriteAllText(Program.file, "Cedula,Nombre,Apellido,Ahorros,Contraseña,Data");
+                foreach (var p in Person.persons)
+                {
+                    File.AppendAllText(Program.file, $"{Environment.NewLine}{p.Id},{p.Name},{p.Apellido},{p.Ahorros},{p.Password},{p.bitpacking}");
+                }
+            }
+        }
+        internal string Insert()
+        {
+            try
+            {
+                if (persons.Contains(this))
+                    return "Ya existe una persona con esta cedula";
+                else
+                {
+                    persons.Add(this);
+                    return "Registro Guardado correctamente";
+                }
+            }
+            catch (Exception)
+            {
+                return "Ha Ocurrido un error";
+            }
+        }
+        internal static Person GetOnePerson(string id)
+            => persons?.Where(a => a.Id.Trim() == id.Trim()).SingleOrDefault();
+
+        internal string UpdateData()
+        {
+            try
+            {
+                var updating = persons.FindIndex(a => a.Id == this.Id);
+
+                persons[updating] = this;
+                return "Datos modificados correctamente";
+            }
+            catch (Exception)
+            {
+                return "No se pudieron modificar los datos";
+            }
+        }
+        internal static int Erase(string id)
+        {
+            var eras = persons.RemoveAll(x => x.Id.Trim() == id.Trim());
+            return eras;
+        }
+    }
+    public enum Gender
+    {
+        Hombre = 0,
+        Mujer = 8
+    }
+    public enum MStatus
+    {
+        Soltero = 0,
+        Casado = 4
+    }
+    public enum AGrad
+    {
+        Inicial = 0,
+        Bachiller = 1,
+        Grado = 2,
+        PostGrado = 3
+    }
+}
---
+++ LabData/LabData/Program.cs
@@ -0,0 +1,481 @@
+﻿using OOP;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace LabWork
+{
+    class Program
+    {
+        public static string file;
+        static void Main(string[] args)
+        {
+            file = @"C:\Users\diego\OneDrive\Desktop\INTEC\Programacion2\TareaLab\Datos.csv";
+            var newFile = !(File.Exists(file));
+            if (newFile)
+            {
+                File.Create(file).Close();
+                File.WriteAllText(file, "Cedula,Nombre,Apellido,Ahorros,Contraseña,Data");
+                Console.WriteLine("Archivo Creado Exitosamente");
+            }
+            else
+            {
+                var list = GetAllData();
+                foreach (var line in list)
+                {
+                    int index = list.IndexOf(line);
+                    if (index > 0)
+                    {
+                        var person = Person.FromCsvLine(line);
+                        Person.persons.Add(person);
+                    }
+                }
+            }
+            int cont = 1;
+            while (cont == 1)
+            {
+                MenuCapt();
+            }
+        }
+        public static string DataCap(bool newFile = true, string oldId = null)
+        {
+            Console.WriteLine("Captura de datos");
+            var id = "";
+            if (newFile)
+            {
+                Console.Write("Cedula: ");
+                id = OnlyInt();
+            }
+            else
+                id = oldId;
+            Console.Write("Nombre: ");
+            var name = Console.ReadLine();
+            Console.Write("Apellido: ");
+            var apellido = Console.ReadLine();
+        ageInput:
+            Console.Write("Edad: ");
+            int age = int.Parse(Console.ReadLine());
+            if (!(age >= 7 && age <= 120))
+                goto ageInput;
+            Console.Write("Contraseña: ");
+            var password = HidePassword();
+            Console.Write("Confirmar Contraseña: ");
+            var confirmPswr = HidePassword();
+            if ((string.IsNullOrEmpty(password)) || !(password.Equals(confirmPswr)))
+            {
+                Console.WriteLine("Las Contraseñas no coinciden");
+            }
+            Console.Write("Ahorros: ");
+            var ahorros = DoubleOnly();
+        generoinput:
+            Console.Write("Sexo M|F: ");
+            int genero;
+            switch (Console.ReadLine().ToUpper())
+            {
+                case "M":
+                    genero = 0;
+                    break;
+                case "F":
+                    genero = 8;
+                    break;
+                default:
+                    goto generoinput;
+            }
+        estadoinput:
+            Console.Write("Estado Civil C|S: ");
+            int estado;
+            switch (Console.ReadLine().ToUpper())
+            {
+                case "C":
+                    estado = 4;
+                    break;
+                case "S":
+                    estado = 0;
+                    break;
+                default:
+                    goto estadoinput;
+            }
+
+        gradoinput:
+            Console.WriteLine("Grado Academico:");
+            Console.WriteLine("Inicial(I)");
+            Console.WriteLine("Bachillerato(B)");
+            Console.WriteLine("Grado(G)");
+            Console.WriteLine("PostGrado(P)");
+            Console.Write("Opcion: ");
+            int grade;
+            switch (Console.ReadLine().ToUpper())
+            {
+                case "I":
+                    grade = 0;
+                    break;
+                case "B":
+                    grade = 1;
+                    break;
+                case "G":
+                    grade = 2;
+                    break;
+                case "P":
+                    grade = 3;
+                    break;
+                default:
+                    goto gradoinput;
+            }
+
+            return $"{id}, {name}, {apellido}, {age}, {ahorros}, {genero}, {estado}, {grade}, {password}";
+        }
+        private static string OnlyInt()
+        {
+            string intVal = "";
+        readingKey:
+            ConsoleKeyInfo consoleKeyInfo = Console.ReadKey(true);
+            while (consoleKeyInfo.Key != ConsoleKey.Enter)
+            {
+                if (consoleKeyInfo.Key != ConsoleKey.Backspace)
+                {
+                    switch (consoleKeyInfo.Key)
+                    {
+                        case ConsoleKey.NumPad0:
+                        case ConsoleKey.D0:
+                            Console.Write("0");
+                            intVal += consoleKeyInfo.KeyChar;
+                            break;
+                        case ConsoleKey.NumPad1:
+                        case ConsoleKey.D1:
+                            Console.Write("1");
+                            intVal += consoleKeyInfo.KeyChar;
+                            break;
+                        case ConsoleKey.NumPad2:
+                        case ConsoleKey.D2:
+                            Console.Write("2");
+                            intVal += consoleKeyInfo.KeyChar;
+                            break;
+                        case ConsoleKey.NumPad3:
+                        case ConsoleKey.D3:
+                            Console.Write("3");
+                            intVal += consoleKeyInfo.KeyChar;
+                            break;
+                        case ConsoleKey.NumPad4:
+                        case ConsoleKey.D4:
+                            Console.Write("4");
+                            intVal += consoleKeyInfo.KeyChar;
+                            break;
+                        case ConsoleKey.NumPad5:
+                        case ConsoleKey.D5:
+                            Console.Write("5");
+                            intVal += consoleKeyInfo.KeyChar;
+                            break;
+                        case ConsoleKey.NumPad6:
+                        case ConsoleKey.D6:
+                            Console.Write("6");
+                            intVal += consoleKeyInfo.KeyChar;
+                            break;
+                        case ConsoleKey.NumPad7:
+                        case ConsoleKey.D7:
+                            Console.Write("7");
+                            intVal += consoleKeyInfo.KeyChar;
+                            break;
+                        case ConsoleKey.NumPad8:
+                        case ConsoleKey.D8:
+                            Console.Write("8");
+                            intVal += consoleKeyInfo.KeyChar;
+                            break;
+                        case ConsoleKey.NumPad9:
+                        case ConsoleKey.D9:
+                            Console.Write("9");
+                            intVal += consoleKeyInfo.KeyChar;
+                            break;
+                    }
+                }
+                else
+                {
+                    if (intVal.Length > 0)
+                    {
+                        intVal = intVal.Remove(intVal.Length - 1);
+                        Console.Write("\b \b");
+                    }
+                }
+                consoleKeyInfo = Console.ReadKey(true);
+            }
+            if (consoleKeyInfo.Key == ConsoleKey.Enter && intVal.Length == 0)
+                goto readingKey;
+            Console.WriteLine();
+            return intVal;
+        }
+        public static string DoubleOnly()
+        {
+            string doubleValue = "";
+            int pointCount = 0;
+            ConsoleKeyInfo key = Console.ReadKey(true);
+            while (key.Key != ConsoleKey.Enter)
+            {
+                if (key.Key != ConsoleKey.Backspace)
+                {
+                    switch (key.Key)
+                    {
+                        case ConsoleKey.NumPad0:
+                        case ConsoleKey.D0:
+                            Console.Write("0");
+                            break;
+                        case ConsoleKey.NumPad1:
+                        case ConsoleKey.D1:
+                            Console.Write("1");
+                            doubleValue += key.KeyChar;
+                            break;
+                        case ConsoleKey.NumPad2:
+                        case ConsoleKey.D2:
+                            Console.Write("2");
+                            doubleValue += key.KeyChar;
+                            break;
+                        case ConsoleKey.NumPad3:
+                        case ConsoleKey.D3:
+                            Console.Write("3");
+                            doubleValue += key.KeyChar;
+                            break;
+                        case ConsoleKey.NumPad4:
+                        case ConsoleKey.D4:
+                            Console.Write("4");
+                            doubleValue += key.KeyChar;
+                            break;
+                        case ConsoleKey.NumPad5:
+                        case ConsoleKey.D5:
+                            Console.Write("5");
+                            doubleValue += key.KeyChar;
+                            break;
+                        case ConsoleKey.NumPad6:
+                        case ConsoleKey.D6:
+                            Console.Write("6");
+                            doubleValue += key.KeyChar;
+                            break;
+                        case ConsoleKey.NumPad7:
+                        case ConsoleKey.D7:
+                            Console.Write("7");
+                            doubleValue += key.KeyChar;
+                            break;
+                        case ConsoleKey.NumPad8:
+                        case ConsoleKey.D8:
+                            Console.Write("8");
+                            doubleValue += key.KeyChar;
+                            break;
+                        case ConsoleKey.NumPad9:
+                        case ConsoleKey.D9:
+                            Console.Write("9");
+                            doubleValue += key.KeyChar;
+                            break;
+
+                        case ConsoleKey.OemPeriod:
+                            if (pointCount == 0)
+                            {
+                                pointCount++;
+                                Console.Write(".");
+                                doubleValue += key.KeyChar;
+                            }
+                            break;
+                    }
+                }
+                else
+                {
+                    if (doubleValue.Length > 0)
+                    {
+                        char character = doubleValue.Last();
+                        if (character == '.')
+                            pointCount = 0;
+                        doubleValue = doubleValue.Remove(doubleValue.Length - 1);
+                        Console.Write("\b \b");
+                    }
+                }
+                key = Console.ReadKey(true);
+            }
+            Console.WriteLine();
+            return doubleValue;
+        }
+
+        public static string HidePassword()
+        {
+            string psw = "";
+            ConsoleKeyInfo key = Console.ReadKey(true);
+            while (key.Key != ConsoleKey.Enter)
+            {
+                if (key.Key != ConsoleKey.Backspace)
+                {
+                    Console.Write("*");
+                    psw += key.KeyChar;
+                }
+                else
+                {
+                    if (psw.Length > 0)
+                    {
+                        psw = psw.Remove(psw.Length - 1);
+                        Console.Write("\b \b");
+                    }
+                }
+                key = Console.ReadKey(true);
+            }
+            Console.WriteLine();
+            return psw;
+        }
+        public static void DataMenu(string record)
+        {
+            Console.WriteLine("Save (S), Reload (R), Exit(E) ");
+            var resp = Console.ReadLine();
+            switch (resp.ToUpper())
+            {
+                case "S":
+                    var person = Person.FromConsole(record);
+                    Console.WriteLine(person.Insert());
+                    Person.SaveToCsv();
+                    break;
+                case "R":
+                    DataMenu(DataCap());
+                    break;
+                case "E":
+                    Environment.Exit(1);
+                    break;
+                default:
+                    DataMenu(record);
+                    break;
+            }
+        }
+        public static void MenuCapt()
+        {
+            Console.WriteLine("1.Capturar");
+            Console.WriteLine("2.Lista");
+            Console.WriteLine("3.Buscar");
+            Console.WriteLine("4.Modificar");
+            Console.WriteLine("5.Eliminar");
+            Console.WriteLine("6.Exit");
+            var op = OnlyInt();
+            switch (op)
+            {
+                case "1":
+                    DataMenu(DataCap());
+                    break;
+                case "2":
+                    foreach (var obj in Person.persons)
+                    {
+                        Console.WriteLine(obj.ToString());
+                    }
+                    break;
+                case "3":
+                    var id = OnlyInt();
+                    var person = Person.GetOnePerson(id)?.ToString();
+                    if (person == null)
+                        Console.WriteLine("No existe la persona");
+                    else
+                        Console.WriteLine(person.ToString());
+                    break;
+                case "4":
+                    ModData();
+                    break;
+                case "5":
+                    EraseData();
+                    break;
+                case "6":
+                    Environment.Exit(1);
+                    break;
+                default:
+                    MenuCapt();
+                    break;
+            }
+        }
+        public static List<string> GetAllData()
+        {
+            string[] datos = File.ReadAllLines(file);
+            List<string> lista = new List<string>();
+            for (int i = 0; i < datos.Length; i++)
+            {
+                lista.Add(datos[i]);
+            }
+            return lista;
+        }
+        public static void ModData()
+        {
+            Console.Write("Digite su cedula: ");
+            var id = OnlyInt();
+
+            var resp = Person.GetOnePerson(id);
+
+            if (resp == null)
+            {
+                Console.WriteLine("No se encontro esta cedula. Desea ingresar otra Y/N? ");
+                var answer = Console.ReadLine();
+                switch (answer.ToUpper())
+                {
+                    case "Y":
+                        ModData();
+                        break;
+                    case "N":
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine(resp.ToString());
+
+                var person = Person.FromConsole(DataCap(false, resp.Id));
+
+                ModMenu(person);
+            }
+        }
+        public static void ModMenu(Person person)
+        {
+            Console.WriteLine("Modificar (M), Reintentar (R), Exit (E) ");
+            var answer = Console.ReadLine();
+            switch (answer.ToUpper())
+            {
+                case "M":
+                    Console.WriteLine(person.UpdateData());
+                    break;
+                case "E":
+                    break;
+                case "R":
+                    ModData();
+                    break;
+            }
+        }
+        public static void EraseData()
+        {
+            Console.Write("Digite la cedula: ");
+            var id = OnlyInt();
+            var resp = Person.GetOnePerson(id);
+
+            if (resp == null)
+            {
+                Console.WriteLine("No se encontro el registro. Desea continuar? Y/N ");
+                var answer = Console.ReadLine();
+                switch (answer.ToUpper())
+                {
+                    case "Y":
+                        EraseData();
+                        break;
+                    case "N":
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine(resp.ToString());
+                DeleteMenu(resp.Id);
+            }
+        }
+
+        public static void DeleteMenu(string id)
+        {
+            Console.WriteLine("Desea borrar este registro? Y/N");
+            var answer = Console.ReadLine();
+            switch (answer.ToUpper())
+            {
+                case "Y":
+                    var cont = Person.Erase(id);
+                    Console.WriteLine($"Se han eliminado ({cont}) registro/s");
+                    break;
+                case "N":
+                    break;
+                default:
+                    DeleteMenu(id);
+                    break;
+            }
+        }
+    }
+}
